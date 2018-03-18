d4f712d5ec07c200d73a98241a907cf90ea9e3a4
jehyun <jhyunpark@yonsei.ac.kr>
2018-03-19 00:36:39 +0900

    revise

--- 5_CRNN/crnn.py
+++ 5_CRNN/crnn.py
@@ -19,15 +19,15 @@ class Config(object):
     def __init__(self, X_train):
         # Input data
         
-        self.train_count = len(X_train)  # 7352 training series
+        self.train_count = len(X_train[0])  # 7352 training series
         # self.test_data_count = len(X_test)  # 2947 testing series
         self.n_steps = 1#len(X_train[0])  # 128 time_steps per series
 
         # Training
         self.learning_rate = 0.0025
         self.lambda_loss_amount = 0.0015
-        self.training_epochs = 100
-        self.batch_size = 90
+        self.training_epochs = 10#100
+        self.batch_size = 10#90
 
         # LSTM structure
         self.n_inputs = 10#len(X_train[0])  # Features count is of 9: 3 * 3D sensors features over time
@@ -102,14 +102,13 @@ if __name__ == "__main__":
     train_x = np.load('./data/train_x.npz')['a']
     train_y = np.load('./data/train_y.npz')['a']
     config = Config(train_x)
-    
-    pred_Y, W, B, weights, biases = CRNN(train_x[0], config)
-    print(pred_Y)
+        
+    x = tf.placeholder(tf.float32, [None, 700*400])
+    y = tf.placeholder(tf.float32,[None, 10]) 
+
+    prediction, W, B, weights, biases = CRNN(train_x, config)
     '''
-    x = tf.placeholder('float', [None, 700*400])
-    y = tf.placeholder('float',[None, 10]) 
-    prediction = c.convolutional_neural_network(x)
-    cost_cnn = tf.reduce_mean( tf.nn.softmax_cross_entropy_with_logits_v2(logits = prediction, labels = y) )
+    cost_cnn = tf.reduce_mean( tf.nn.softmax_cross_entropy_with_logits_v2(labels = y, logits = prediction) )
     optimizer = tf.train.AdamOptimizer().minimize(cost_cnn)
     
     hm_epochs = 10
@@ -127,22 +126,18 @@ if __name__ == "__main__":
         # print('Accuracy:',accuracy.eval({x:mnist.test.images, y:mnist.test.labels}))
     ###################################################################################################
     
-    config = Config(train_x)
 
     X = tf.placeholder(tf.float32, [None, config.n_steps, config.n_inputs])
     Y = tf.placeholder(tf.float32, [None, config.n_classes])    
-    
-    pred_Y, W, B = LSTM_Network(X, config)
+    '''
+    prediction, W, B, weights, biases = CRNN(train_x, config)
     # Loss,optimizer,evaluation
-    l2 = config.lambda_loss_amount * 
-        sum(tf.nn.l2_loss(tf_var) for tf_var in tf.trainable_variables())
+    l2 = config.lambda_loss_amount * sum(tf.nn.l2_loss(tf_var) for tf_var in tf.trainable_variables())
     # Softmax loss and L2
-    cost = tf.reduce_mean(
-        tf.nn.softmax_cross_entropy_with_logits(labels=Y, logits=pred_Y)) + l2
-    optimizer = tf.train.AdamOptimizer(
-        learning_rate=config.learning_rate).minimize(cost)
+    cost = tf.reduce_mean( tf.nn.softmax_cross_entropy_with_logits(labels=y, logits=prediction) ) + l2
+    optimizer = tf.train.AdamOptimizer(learning_rate=config.learning_rate).minimize(cost)
 
-    correct_pred = tf.equal(tf.argmax(pred_Y, 1), tf.argmax(Y, 1))
+    correct_pred = tf.equal(tf.argmax(prediction, 1), tf.argmax(y, 1))
     accuracy = tf.reduce_mean(tf.cast(correct_pred, dtype=tf.float32))
 
     sess = tf.InteractiveSession(config=tf.ConfigProto(log_device_placement=False))
@@ -154,35 +149,38 @@ if __name__ == "__main__":
     for i in range(config.training_epochs):
         for start, end in zip(range(0, config.train_count, config.batch_size),
                               range(config.batch_size, config.train_count + 1, config.batch_size)):
-            sess.run(optimizer, feed_dict={X: X_train[start:end],
-                                           Y: y_train[start:end]})
+            sess.run(optimizer, feed_dict={x: train_x[start:end],
+                                           y: train_y[start:end]})
 
         # Test completely at every epoch: calculate accuracy
-        pred_out, accuracy_out, loss_out, weight_trained, biases_trained = sess.run(
-            [pred_Y, accuracy, cost, W, B],
-            feed_dict={
-                X: X_test,
-                Y: y_test
-            }
+        pred_out, accuracy_out, loss_out, W, B, weights, biases = sess.run(
+            [prediction, accuracy, cost, W, B, weights, biases], feed_dict={x: train_x, y: train_y}
         )
-        
-    
-    
+
         print("training iter: {},".format(i) +
               " test accuracy : {},".format(accuracy_out) +
               " loss : {}".format(loss_out))
         best_accuracy = max(best_accuracy, accuracy_out)
     
-    '''
+    
     '''save weights and biases'''
-    '''
-    np.save('./weights/weight_hidden1',weight_trained['hidden'])
-    np.save('./weights/weight_output1',weight_trained['output'])
-    np.save('./weights/biases_hidden1',biases_trained['hidden'])
-    np.save('./weights/biases_output1',biases_trained['output'])
+    
+    np.savez_compressed('./data/W_hidden',W['hidden'])
+    np.savez_compressed('./data/W_output',W['output'])
+    np.savez_compressed('./data/b_hidden',B['hidden'])
+    np.savez_compressed('./data/b_output',B['output'])
+    np.savez_compressed('./data/W_conv1',weights['W_conv1'])
+    np.savez_compressed('./data/W_conv2',weights['W_conv2'])
+    np.savez_compressed('./data/W_fc',weights['W_fc'])
+    np.savez_compressed('./data/W_out',weights['out'])
+    np.savez_compressed('./data/b_conv1',biases['b_conv1'])
+    np.savez_compressed('./data/b_conv2',biases['b_conv2'])
+    np.savez_compressed('./data/b_fc',biases['b_fc'])
+    np.savez_compressed('./data/b_out',biases['out'])
+
 
     print("")
     print("final test accuracy: {}".format(accuracy_out))
     print("best epoch's test accuracy: {}".format(best_accuracy))
     print("")
-    '''
+    
