aa0f6366e6ce62e4a49d3c00b248d7cd005d204d
Jehyun Park <jhyunpark@yonsei.ac.kr>
2018-03-18 18:53:05 +0900

    revise

--- 5_CRNN/crnn.py
+++ 5_CRNN/crnn.py
@@ -17,7 +17,7 @@ def train_neural_network(x,y):
     c = conv.conv()
 
     prediction = c.convolutional_neural_network(x)
-    cost = tf.reduce_mean( tf.nn.softmax_cross_entropy_with_logits_v2(logits = prediction, labels = y) )
+    cost = tf.reduce_mean( tf.nn.softmax_cross_entropy_with_logits(logits = prediction, labels = y) )
     optimizer = tf.train.AdamOptimizer().minimize(cost)
     
     hm_epochs = 10
