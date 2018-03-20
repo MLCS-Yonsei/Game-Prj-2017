7930bdb79ca561a5c53781fe4d9a1572bf3c789d
Jehyun Park <jhyunpark@yonsei.ac.kr>
2018-03-20 23:09:59 +0900

    revise

--- 5_CRNN/crnn.py
+++ 5_CRNN/crnn.py
@@ -144,7 +144,7 @@ if __name__ == "__main__":
     
     X = tf.placeholder(tf.float32, [None, config.n_steps, config.n_inputs])
     Y = tf.placeholder(tf.float32, [None, config.n_classes])    
-    
+    '''
     prediction, label, W, B, weights, biases = CRNN(X, Y, config)
     # Loss,optimizer,evaluation
     l2 = config.lambda_loss_amount * sum(tf.nn.l2_loss(tf_var) for tf_var in tf.trainable_variables())
@@ -208,7 +208,7 @@ if __name__ == "__main__":
     
     
     sess.close()
-    '''
+    
     '''save weights and biases'''
     
  
