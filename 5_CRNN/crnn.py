988dec9b6f42b322f7dc187939d37c32e36aa99c
Jehyun Park <jhyunpark@yonsei.ac.kr>
2018-03-26 17:22:38 +0900

    revise

--- 5_CRNN/crnn.py
+++ 5_CRNN/crnn.py
@@ -27,7 +27,7 @@ class Config(object):
         self.learning_rate = 0.0025
         self.lambda_loss_amount = 0.0015
         self.training_epochs = 10#100
-        self.batch_size = 50#90
+        self.batch_size = 5#90
 
         # LSTM structure
         self.n_inputs = 10#len(X_train[0])  # Features count is of 9: 3 * 3D sensors features over time
@@ -105,7 +105,7 @@ def CRNN(_X, _Y, config):
 
 if __name__ == "__main__":
     train_x = np.load('./data/train_x.npz')['a']#[:50,:]
-    train_x = np.reshape(train_x,[-1,10,280000])
+    train_x = np.reshape(train_x,[-1,10,1262*720])
     train_y = np.load('./data/train_y.npz')['a']#[:5,:]
     config = Config(train_x)
     X = tf.placeholder(tf.float32, [None,10, config.img_h*config.img_w])
@@ -141,7 +141,7 @@ if __name__ == "__main__":
             for start, end in zip(range(0, config.train_count, config.batch_size),
                                 range(config.batch_size, config.train_count + 1, config.batch_size)):
                 sess.run(optimizer, feed_dict={X: train_x[start:end],
-                                            Y: train_y[start/10:end/10]})
+                                            Y: train_y[start:end]})
             
             # Test completely at every epoch: calculate accuracy
             pred_out, accuracy_out, loss_out, W_, B_, weights_, biases_ = sess.run(
