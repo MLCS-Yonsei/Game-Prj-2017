672a9b8d802e34fa2445798951554a2f7280700a
Jehyun Park <jhyunpark@yonsei.ac.kr>
2018-04-05 13:31:14 +0900

    revise

--- 5_CRNN/cnn.py
+++ 5_CRNN/cnn.py
@@ -25,7 +25,7 @@ class Config(object):
         # Training
         self.learning_rate = 0.01#0.0025
         self.lambda_loss_amount = 0.0015
-        self.training_epochs = 10000
+        self.training_epochs = 5000
         self.batch_size = 10#90
 
         # LSTM structure
