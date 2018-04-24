3b1d054a2a641638298463c411f226c23fefb229
Jehyun Park <jhyunpark@yonsei.ac.kr>
2018-04-24 16:21:13 +0900

    dd

--- 5_CRNN/crnn_test_v2.py
+++ 5_CRNN/crnn_test_v2.py
@@ -178,5 +178,5 @@ if __name__ == "__main__":
     # sess = tf.InteractiveSession(config=tf.ConfigProto(log_device_placement=False))
     
 
-  print(prediction,np.argmax(prediction))
+  print(prediction.eval(),np.argmax(prediction))
   
