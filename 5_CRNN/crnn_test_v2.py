1f40dc9ee8c4202ad940fa44954b2ff890e75f2c
Jehyun Park <jhyunpark@yonsei.ac.kr>
2018-04-24 16:53:10 +0900

    dd

--- 5_CRNN/crnn_test_v2.py
+++ 5_CRNN/crnn_test_v2.py
@@ -133,9 +133,7 @@ def LSTM(_X, config):
 if __name__ == "__main__":
   graph, bottleneck_tensor, jpeg_data_tensor, resized_image_tensor = (
         create_inception_graph())
-  config = Config()
 
-   
   with tf.Session(graph=graph) as sess:
     init = tf.global_variables_initializer()
     sess.run(init)
@@ -148,16 +146,20 @@ if __name__ == "__main__":
       elif len(frames) < 10:
         jpeg_data = gfile.FastGFile(full_filename, 'rb').read()
         frames = np.concatenate((frames, run_bottleneck_on_image(sess, jpeg_data, jpeg_data_tensor, bottleneck_tensor)[np.newaxis,:]), axis = 0)
+  sess.close()
 
-  frames = tf.transpose(frames[np.newaxis,:,:], [1, 0, 2])  # permute n_steps and batch_size
+  config = Config()
+
+  X = tf.placeholder(tf.float32, [None, config.n_steps, config.n_inputs])
+  X = tf.transpose(X[np.newaxis,:,:], [1, 0, 2])  # permute n_steps and batch_size
   # Reshape to prepare input to hidden activation
-  frames = tf.reshape(frames, [-1, config.n_inputs])
+  X = tf.reshape(X, [-1, config.n_inputs])
   # new shape: (n_steps*batch_size, n_input)
 
   # Linear activation
-  frames = tf.nn.relu(tf.matmul(frames, config.W['hidden']) + config.biases['hidden'])
+  X = tf.nn.relu(tf.matmul(X, config.W['hidden']) + config.biases['hidden'])
   # Split data because rnn cell needs a list of inputs for the RNN inner loop
-  frames = tf.split(frames, config.n_steps, 0)
+  X = tf.split(X, config.n_steps, 0)
   # new shape: n_steps * (batch_size, n_hidden)
 
   # Define two stacked LSTM cells (two recurrent layers deep) with tensorflow
@@ -165,17 +167,25 @@ if __name__ == "__main__":
   lstm_cell_2 = tf.contrib.rnn.BasicLSTMCell(config.n_hidden, forget_bias=1.0, state_is_tuple=True)
   lstm_cells = tf.contrib.rnn.MultiRNNCell([lstm_cell_1, lstm_cell_2], state_is_tuple=True)
   # Get LSTM cell output
-  outputs, states = tf.contrib.rnn.static_rnn(lstm_cells, frames, dtype=tf.float32)
+  outputs, states = tf.contrib.rnn.static_rnn(lstm_cells, X, dtype=tf.float32)
 
   # Get last time step's output feature for a "many to one" style classifier,
   # as in the image describing RNNs at the top of this page
   lstm_last_output = outputs[-1]
 
   # Linear activation
-  prediction = tf.matmul(lstm_last_output, config.W['output']) + config.biases['output']    
-      
-    # sess = tf.InteractiveSession(config=tf.ConfigProto(log_device_placement=False))
-    
+  pred_out = tf.matmul(lstm_last_output, config.W['output']) + config.biases['output']    
 
-  print(prediction.eval(),np.argmax(prediction))
-  sess.close()
+  sess = tf.InteractiveSession(config=tf.ConfigProto(log_device_placement=False))
+  init = tf.global_variables_initializer()
+  sess.run(init)    
+
+  prediction = sess.run(
+            [pred_out],
+            feed_dict={
+                X: frames,
+                            }
+        )
+
+  print(np.argmax(prediction))
+  
