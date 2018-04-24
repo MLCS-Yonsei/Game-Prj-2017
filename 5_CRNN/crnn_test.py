b414e78f087e375a037e2d47fa72a21be78842ab
Jehyun Park <jhyunpark@yonsei.ac.kr>
2018-04-24 14:44:18 +0900

    dd

--- 5_CRNN/crnn_test.py
+++ 5_CRNN/crnn_test.py
@@ -23,7 +23,7 @@ from tensorflow.python.platform import gfile
 # from tensorflow.python.util import compat
 
 model_dir = '/home/jehyunpark/Downloads/crnn/results/'
-image_path = '/home/jehyunpark/Downloads/crnn/images/boxing/'
+image_path = '/home/jehyunpark/Downloads/crnn/images/handwaving/'
 
 
 BOTTLENECK_TENSOR_NAME = 'pool_3/_reshape:0'
